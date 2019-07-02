5affbbc83ca9a8878a7b6610113402c369f81df5
leamsi4ever <50880890+leamsi4ever@users.noreply.github.com>
2019-07-02 15:13:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+liClick();
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+	liClick();
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+
+
+function liClick(){
+	for(var i = 0; i < li.length; i++) {
+	li[i].addEventListener('click', changeClass)
+	}
+}
+
+function changeClass(){
+	console.log(this);
+	this.classList.toggle('done');
+}
